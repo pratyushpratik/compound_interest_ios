fb5714c86041f9f359e5ac21a385b45738f03afe
Sierra 4 <sierra4@OSX16.local>
2017-01-30 11:46:03 +0530

    initial commit

--- compoundInterest/ViewController.swift
+++ compoundInterest/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var txtPricipal: UITextField!
+    
+    
+    @IBOutlet weak var txtRate: UITextField!
+    
+    @IBOutlet weak var txtTime: UITextField!
+    
+    @IBOutlet weak var lblCompoundInterest: UILabel!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+   
+
+    
+    @IBAction func btnClick1(_ sender: Any) {
+        
+        guard let value1 = Float(txtPricipal.text ?? "0") else {
+            return
+        }
+        
+        guard let value2 = Float(txtRate.text ?? "0") else {
+            return
+        }
+        
+        guard let value3 = Float(txtTime.text ?? "0") else {
+            return
+        }
+        let value4 = 1 + value2
+        
+        let result = value1 * (pow(value4, value3))
+        
+        lblCompoundInterest.text = "\(result)"
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
